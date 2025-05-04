a90981ebff8572e7a80d54c30c4d6320ea0bd40c
Shruti Srivastava <101975373+Shrutisri31@users.noreply.github.com>
2025-05-04 18:54:08 +0000

    conversion of datatypes

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -1,2 +1,38 @@
 //Lecture-3
 
+// let z = "33";
+// console.log(typeof z);
+// console.log(typeof (z));
+
+// let valueInNumber =  Number(z);
+// console.log(typeof(valueInNumber));
+
+// let x = "12asd";
+// console.log(typeof(x)); //String
+// let valueInNum = Number(x);
+// console.log(typeof(valueInNum)); //number
+// console.log(valueInNum);  //NaN value (Not a number)
+
+// let y = null;
+// console.log(typeof(y)); //object
+// console.log(y);   //null
+// let valueInN = Number(y);
+// console.log(typeof(valueInN)); // number
+// console.log(valueInN);   //0
+
+// Number - Boolean
+let isLoggedIn = 1;
+console.log(typeof(isLoggedIn));
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+console.log(typeof(booleanIsLoggedIn));
+
+
+
+
+
+
+
+
+
+

90d69352774b329c8bb93bb92765181582d2e3d5
Shruti Srivastava <101975373+Shrutisri31@users.noreply.github.com>
2025-05-04 19:14:46 +0000

    operations

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -21,11 +21,36 @@
 // console.log(valueInN);   //0
 
 // Number - Boolean
+/*
 let isLoggedIn = 1;
 console.log(typeof(isLoggedIn));
 let booleanIsLoggedIn = Boolean(isLoggedIn);
 console.log(booleanIsLoggedIn);
 console.log(typeof(booleanIsLoggedIn));
+*/
+
+/* datatypes of variables can be converted into different datatypes: 
+1. String() 2. Number(), 3. Boolean();
+*/
+
+//**********Operations***************/
+
+let str1 = "Hi";
+let str2 = " World";
+let str3 = str1 + str2;
+// console.log(str3);
+
+console.log(1 + 2); //3
+console.log("1" + 2); //12
+console.log(1 + "2"); //12
+console.log("1" + 2 + 2); //122
+console.log(1 + 2 + "2"); //32
+
+
+
+
+
+
 
 
 
